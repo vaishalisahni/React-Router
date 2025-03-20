3022bcfd7733770a01917c85acf0ea2b0a1032ce
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-03-20 19:47:29 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,69 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 // import App from './App.jsx'
-import Home from './components/Home/Home.jsx'
-import About from './About/About.jsx'
-import Layout from './Layout.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Contact from './Contact/Contact.jsx'
+import Home from "./components/Home/Home.jsx";
+import About from "./About/About.jsx";
+import Layout from "./Layout.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Routes,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Contact from "./Contact/Contact.jsx";
+import { createFilter } from "vite";
 
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path:"about",
-        element: <About />
-      },
-      {
-        path:"contact",
-        element:<Contact />
-      }
-
-    ]
-  }
-])
-// const router = createBrowserRouter
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home />} />
+      <Route path="about" element={<About /> } />
+      <Route path="contact" element={<Contact /> } />
+    </Route>
+  )
 )
+
+createRoot(document.getElementById("root")).render(
+  
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+// <BrowserRouter>
+    {/* <RouterProvider router={router}/> */}
+    {/* <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes> */}
+    {/* <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter> */}
