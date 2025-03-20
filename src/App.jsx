d3af229feb6e641bd32a3477572d42a92fc4de26
Eshank Tyagi <eshank@optiq.ai>
2025-03-20 23:50:55 +0530

    ... added back

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/footer'
+import Home from './components/Home/Home'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,97 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.jsx'
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Layout from "./Layout.jsx";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Routes,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Contact from "./components/Contact/Contact.jsx";
+// import { createFilter } from "vite";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
+import User from "./components/User/User.jsx";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route loader={githubInfoLoader}
+       path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//      <RouterProvider router={router}/>
+//    </StrictMode>
+//  )
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
+// <BrowserRouter>
+{
+  /* <RouterProvider router={router}/> */
+}
+{
+  /* <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes> */
+}
+{
+  /* <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter> */
+}
