6ebbe141f975bf591e8a87369e90d371c7aa0a8a
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-03-20 23:24:25 +0530

    added

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        fetch('https://api.github.com/users/vaishalisahni')
-        .then(response=>response.json())
-        .then(data=> {
-            console.log(data);
-            setData(data)
-    })
-    },[])
+    const data=useLoaderData()
+    // const [data,setData]=useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/vaishalisahni')
+    //     .then(response=>response.json())
+    //     .then(data=> {
+    //         console.log(data);
+    //         setData(data)
+    // })
+    // },[])
   return (
     <div className='text-center m-4 bg-grey-600 text-white p-4 text-3xl '>
       Github followers: {data.followers}
@@ -19,3 +21,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader= async ()=>{
+    const response = await fetch('https://api.github.com/users/vaishalisahni');
+    return response.json()
+}
